9c3d97cbc0eb6f32959193fa0723b0137c796b97
CarinaGolberg <karina.birs@gmail.com>
2025-10-26 16:27:56 +0700

    Add files via upload

---
+++ IDomainObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEM3_PR1_MODEL
+{
+    public interface IDomainObject
+    {
+        int Id { get; set; }
+    }
+}
